e68adf46e8119752babdb4c6b28a8b83d1a265b3
ianchisionut <ianchisionut@gmail.com>
2026-08-16 20:12:47 +0300

    Onboarding pe dashboard + fix searchParams Next 16

--- next-level-agency/app/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AuthForm } from "@/app/components/auth/auth-form";
-
-export default function LoginPage() {
-  return <AuthForm mode="login" />;
-}
--- next-level-agency/app/signup/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AuthForm } from "@/app/components/auth/auth-form";
-
-export default function SignupPage() {
-  return <AuthForm mode="signup" />;
-}
